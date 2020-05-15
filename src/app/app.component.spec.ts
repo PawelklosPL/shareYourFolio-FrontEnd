82e0b6bda5c1da212be4d2d87fd5606a04a67437
lecytynaSojowa <50423642+lecytynaSojowa@users.noreply.github.com>
2020-05-15 18:06:17 +0200

    tests tests tests
    basic tests have been added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,29 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'share-your-folio-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('share-your-folio-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('share-your-folio-app app is running!');
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AppComponent],
+            schemas: [
+                CUSTOM_ELEMENTS_SCHEMA
+            ],
+        })
+            .compileComponents();
+    }));
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+    it('should create', () => {
+        expect(component).toBeDefined();
+    });
+    it('title-match', () => {
+        expect(component.title).toEqual('share-your-folio-app');
+    });
 });
--- src/app/user-project/avatar/remove-avatar/remove-avatar.component.spec.ts
+++ src/app/user-project/avatar/remove-avatar/remove-avatar.component.spec.ts
@@ -1,25 +1,61 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RemoveAvatarComponent } from './remove-avatar.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { Avatar } from '../avatar.model';
+import { Table } from '../view-avatar/view-avatar.model';
 
 describe('RemoveAvatarComponent', () => {
-  let component: RemoveAvatarComponent;
-  let fixture: ComponentFixture<RemoveAvatarComponent>;
+    let component: RemoveAvatarComponent;
+    let fixture: ComponentFixture<RemoveAvatarComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RemoveAvatarComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                RemoveAvatarComponent
+            ],
+            imports: [
+                NgbModule,
+                FormsModule,
+                HttpClientModule
+            ],
+            schemas: [
+                CUSTOM_ELEMENTS_SCHEMA
+            ],
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RemoveAvatarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        })
+            .compileComponents();
+    }));
+    beforeEach(() => {
+        fixture = TestBed.createComponent(RemoveAvatarComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        let avatar: Avatar = {
+            AuthorId: '0', AuthorName: '',
+            CommentNumber: 0,
+            Comment_Id: 0,
+            Description: '',
+            FolderName: '',
+            Id: 0, ImagesUrl: ['', ''],
+            Name: 'daj mi kawy',
+            PublishDate: new Date(),
+            SharePoints: 0,
+            Tags: ['', '']
+        };
+        let table: Table = {
+            avatar: avatar,
+            isSelected: true
+        };
+        component.row = table;
+    });
+    it('should create', () => {
+        expect(component).toBeDefined();
+    });
+    it('name', () => {
+        let button = fixture.debugElement.nativeElement.querySelector('button');
+        button.click();
+    });
 });
+
